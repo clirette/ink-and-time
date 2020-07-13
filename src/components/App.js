530fed707e9c048a2e7a10dc379f8207c208e294
Chase Lirette <chaselirette@gmail.com>
2020-07-13 18:42:04 -0500

    Added coming soon image.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,7 @@
 import React from 'react'
 import '../css/global.css'
+import ComingSoon from './ComingSoon'
 
-const App = () => {
-  return (
-    <div>
-      Ink &amp; Time Coming Soon!
-    </div>
-  )
-}
+const App = () => <ComingSoon />
 
 export default App
---
+++ src/components/ComingSoon.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+import comingSoon from '../images/inkandtimecomingsoon.png'
+const ComingSoon = styled.div`
+  background-image: url(${comingSoon});
+  background-repeat:no-repeat;
+  background-attachment:fixed;
+  background-position:100% 100%;
+  background-size:cover;  
+  height: 100vh;
+  width: 100vw;
+`
+
+export default ComingSoon
